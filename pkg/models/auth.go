eeba3a6d6a2e31905a20f15869bfff41c3b94f42
Ronnie Zad Muhanguzi <zadcorna@gmail.com>
2023-01-04 05:43:58 +0300

    changes

--- pkg/models/auth.go
+++ pkg/models/auth.go
@@ -3,6 +3,8 @@ package models
 
 type User struct {
     Id       int64  `json:"id" gorm:"primaryKey"`
+    PhoneNumber    string `json:"phoneNumber"`
+    Name    string `json:"name"`
     Email    string `json:"email"`
     Password string `json:"password"`
 }
