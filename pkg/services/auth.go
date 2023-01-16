961b1b147899bb1feaa00a4fe0183e1048701faf
Ronnie Zad Muhanguzi <zadcorna@gmail.com>
2023-01-16 18:42:28 +0300

    refactor

--- pkg/services/auth.go
+++ pkg/services/auth.go
@@ -2,11 +2,11 @@ package services
 
 import (
 	"context"
-	"log"
 	"github.com/RonnieZad/nyumba-go-grpc-auth-svc/pkg/db"
 	"github.com/RonnieZad/nyumba-go-grpc-auth-svc/pkg/models"
 	"github.com/RonnieZad/nyumba-go-grpc-auth-svc/pkg/pb"
 	"github.com/RonnieZad/nyumba-go-grpc-auth-svc/pkg/utils"
+
 	"net/http"
 )
 
@@ -22,31 +22,24 @@ func (*Server) mustEmbedUnimplementedAuthServiceServer() {
 }
 
 func (s *Server) Register(ctx context.Context, req *pb.RegisterRequest) (*pb.RegisterResponse, error) {
-	// var userDetails models.User
+	var userDetails models.User
 
-	// if result := s.H.DB.Where(&models.User{Email: req.Email}).First(&userDetails); result.Error == nil {
-	// 	return &pb.RegisterResponse{
-	// 		Status: http.StatusConflict,
-	// 		Error:  "User already exists",
-	// 	}, nil
-	// }
-	// user := &models.User{
-	// 	Name:        req.Name,
-	// 	Email:       req.Email,
-	// 	PhoneNumber: req.PhoneNumber,
-	// 	Password:    utils.HashPassword(req.Password),
-	// }
-	log.Printf("Received register request: %s", req)
+	if result := s.H.DB.Where(&models.User{EmailAddress: req.EmailAddress}).First(&userDetails); result.Error == nil {
+		return &pb.RegisterResponse{
+			Status: http.StatusConflict,
+			Error:  "User already exists",
+		}, nil
+	}
 
-	// user := &models.User{Name: "Ronnie Zad", EmailAddress: "zadcorna@gmail.com", PhoneNumber: "+256702703612", Password: utils.HashPassword(req.Password)}
-	// s.H.DB.Create(&models.User{
-	// 	Name:        "req.Name",
-	// 	Email:       req.Email,
-	// 	PhoneNumber: req.PhoneNumber,
-	// 	Password:    utils.HashPassword(req.Password),
-	// })
 
-	// s.H.DB.Create(&user)
+	user := &models.User{
+		ID: "2",
+		Name: req.Name, EmailAddress: req.EmailAddress, PhoneNumber: req.PhoneNumber, Password: utils.HashPassword(req.Password),
+		DateOfBirth: req.DateOfBirth, CreditScore: req.CreditScore, IsFinanceWorthy: req.IsFinanceWorthy, WorkPlace: req.WorkPlace, NIN: req.Nin, EmployerName: req.EmployerName,
+		SalaryScale: req.SalaryScale, IsKYCVerified: req.KycVerified,
+	}
+
+	s.H.DB.Create(&user)
 
 	return &pb.RegisterResponse{
 		Status: http.StatusCreated,
@@ -56,7 +49,7 @@ func (s *Server) Register(ctx context.Context, req *pb.RegisterRequest) (*pb.Reg
 func (s *Server) Login(ctx context.Context, req *pb.LoginRequest) (*pb.LoginResponse, error) {
 	var user models.User
 
-	if result := s.H.DB.Where(&models.User{EmailAddress: req.Email}).First(&user); result.Error != nil {
+	if result := s.H.DB.Where(&models.User{EmailAddress: req.EmailAddress}).First(&user); result.Error != nil {
 		return &pb.LoginResponse{
 			Status: http.StatusNotFound,
 			Error:  "User account not found",
